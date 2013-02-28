0d2636a573274975721db904ec52c30bc5408fa1
Kevin Carmody <kevin@skinofstars.com>
2013-02-28 23:11:47 +0000

    just some scraps i'm playing with

---
+++ examples/crc.js
@@ -0,0 +1,43 @@
+var crc = require('crc');
+//console.log('cock')
+
+var Buffer = require('buffer').Buffer;
+
+var buf = new Buffer(5);
+// buf = [ 0x01,                  // command
+//         0x00,                  // uh, there was an extra NULL in the wiresharp dump, so...
+//         0x1d,                  // protocol version (29)
+//         this.name.length,      // username length
+//         this.name,             // my username
+//         0x00, 0x00, 0x00, 0x00, 0x00, 0x00    // unused
+//     ];
+
+//buf[0] ="0xAA";  // frame prefix
+//buf[1] ="0xBB";  // frame prefix
+buf[0] = "0x01";  // CMD
+buf[1] = "0x03";  // LEN
+buf[2] = "0x00";  // device type - PC
+buf[3] = "0x00";  // version major
+buf[4] = "0x01";  // version minor
+
+// var str = 0;
+// for (b in buf) {
+//     console.log(b)
+//     console.log('int: '+parseInt(b, 16));
+//     str += parseInt(b, 16)
+// }
+
+// console.log(str);
+
+//buf[7] ="0x21";  // CRC
+//buf[8] ="0xAC";  // CRC
+
+var myCRC = crc.buffer.crc16(buf);
+
+console.log(myCRC)
+
+console.log(myCRC.toString(16))
+
+var str = "01 03 00 00 01";
+var num = 0103000001;
+console.log(crc.crc16(str))
---
+++ examples/discovery.js
@@ -0,0 +1,55 @@
+// get local ip address
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family == 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+
+var Buffer = require('buffer').Buffer;
+var dgram = require('dgram');
+
+// set up receiver
+var log = require('sys').log;
+var receivesock = dgram.createSocket("udp4", function(msg, rinfo){
+    log('got message ' + rinfo.address + ':' + rinfo.port);
+    log('data ln: ' + rinfo.size + " data: " + msg.toString('hex'))
+});
+receivesock.bind(25561, addresses[0]);
+
+//.toString('ascii', 0, rinfo.size)
+
+// set up sender
+// 
+// The BCSD message is intended to be broadcasted
+// Command Code (CMD):0x01
+// Message Body Length (LEN): 3
+// Fields in the message body: 
+//  1. [0] Sys (1 byte): Transmitter system type (0:PC client, 1: iPhone, 2: Android)
+//  2. [1..2] Version (2 bytes): Version number of the transmitter first byte is the major version and the second is the minor version number.
+// CRC
+var buf = new Buffer(9);
+
+buf[0] ="0xAA";  // frame prefix
+buf[1] ="0xBB";  // frame prefix
+buf[2] ="0x01";  // CMD
+buf[3] ="0x03";  // LEN
+buf[4] ="0x00";  // device type - PC
+buf[5] ="0x00";  // version major
+buf[6] ="0x01";  // version minor
+buf[7] ="0x21";  // CRC = 33
+buf[8] ="0xAC";  // CRC
+
+var sendsock = dgram.createSocket("udp4");
+sendsock.bind(25561);
+sendsock.setBroadcast(true);
+sendsock.send(buf, 0, 9, 1984, "255.255.255.255", function(err, bytes){
+    sendsock.close();
+});
+
+
