2169e1156e0f5d4769dc113147843d2b8cd1838e
Kevin Carmody <kevin@skinofstars.com>
2013-03-19 18:52:17 +0000

    emitter wait

--- examples/wirc-basic-control.js
+++ examples/wirc-basic-control.js
@@ -3,23 +3,21 @@
 var Client = require('../lib/Client');
 
 var client = new Client();
-
-client
-    .resume();
     
 client
-    .after(50, function(){
+    .resume()
+    .after(5000, function(){
         this.forward(0.2);
     })
-    .after(1000, function(){
-        this.left(0.5);
-        this.backward(0.4);
-    })
-    .after(1000, function(){
-        this.stop();
-    })
-    .after(500, function(){
-        this.finish();
-    });
+    // .after(1000, function(){
+    //     this.left(0.5);
+    //     this.backward(0.4);
+    // })
+    // .after(1000, function(){
+    //     this.stop();
+    // })
+    // .after(500, function(){
+    //     this.finish();
+    // });
 
 
